c6501e4255a20300161fa949f2c82f71f94f38ea
koray.beyaz73@gmail.com <koray.beyaz73@gmail.com>
2019-01-03 18:48:12 +0300

    Stop tracking

--- TRENDYOL/build/lib/TRENDYOL/spiders/decorators.py
+++
@@ -1,7 +0,0 @@
-def count_pages(func):
-    "Decorator to count the number of times a function is called. Used for incrementing the pages."
-    def wrapper(*args, **kwargs):
-        wrapper.counter += 1
-        func(*args, **kwargs)
-    wrapper.counter = 0
-    return wrapper
